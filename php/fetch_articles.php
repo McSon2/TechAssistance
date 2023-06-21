ac8b9c639ab5d92cc2cb3ae1e41a137fd2b66b02
McSon2 <contact@techassistance.fr>
2023-06-21 20:05:19 +0200

    modified:   php/fetch_articles.php

--- php/fetch_articles.php
+++ php/fetch_articles.php
@@ -1,7 +1,7 @@
 <?php
-require_once 'db_config.php';
-
 try {
+    require_once 'db_config.php';
+
     $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -9,8 +9,8 @@ try {
     $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     echo json_encode($articles);
-} catch(PDOException $e) {
-    error_log("Erreur lors de la récupération des articles : " . $e->getMessage());
+} catch(Exception $e) {
+    error_log("Erreur : " . $e->getMessage());
     echo json_encode(array('error' => 'Une erreur est survenue.', 'message' => $e->getMessage()));
 }
 ?>
