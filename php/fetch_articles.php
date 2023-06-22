08d75c104dd180a0778c195edbbc8a5a8230cf91
McSon2 <contact@techassistance.fr>
2023-06-22 09:57:36 +0200

    modified:   php/fetch_articles.php

--- php/fetch_articles.php
+++ php/fetch_articles.php
@@ -1,21 +1,18 @@
 <?php
-try {
-    require_once "db_config.php";
+require_once 'db_config.php';
 
-    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $stmt = $db->query("SELECT * FROM articles");
-$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-if ($articles) {
+    header('Content-Type: application/json');
     echo json_encode($articles);
-} else {
-    echo json_encode(array('error' => 'Aucun article trouvé.'));
-}
-
-} catch(Exception $e) {
-    error_log("Erreur : " . $e->getMessage());
-    echo json_encode(array('error' => 'Une erreur est survenue.', 'message' => $e->getMessage()));
+} catch(PDOException $e) {
+    error_log("Erreur lors de la récupération des articles : " . $e->getMessage());
+    header('Content-Type: application/json');
+    echo json_encode(array('error' => 'Une erreur est survenue.'));
 }
 ?>
