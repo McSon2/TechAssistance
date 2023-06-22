5b9053891d7fbf814d04671725d37eff21946d7f
McSon2 <contact@techassistance.fr>
2023-06-22 16:08:48 +0200

    modified:   php/fetch_article.php

--- php/fetch_article.php
+++ php/fetch_article.php
@@ -13,20 +13,20 @@ try {
     $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Avant la requête à la base de données\n";
     $stmt = $db->prepare("SELECT * FROM articles WHERE id = :id");
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
 
-    echo "Après la requête à la base de données\n";
     $article = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    var_dump($article);
-
-    header('Content-Type: application/json');
     if ($article) {
+        echo "Avant l'envoi de l'article\n";
+        header('Content-Type: application/json');
         echo json_encode($article);
+        echo "\nAprès l'envoi de l'article\n";
     } else {
+        echo "Aucun article trouvé avec cet ID.\n";
+        header('Content-Type: application/json');
         echo json_encode(['error' => 'Aucun article trouvé avec cet ID.']);
     }
 } catch(Exception $e) {
