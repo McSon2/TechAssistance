fcdabc99de38dfa39461c0aaedce5ec76b4951ea
McSon2 <contact@techassistance.fr>
2023-06-23 17:06:51 +0200

    modified:   css/blog.css 	modified:   js/blog.js

--- js/blog.js
+++ js/blog.js
@@ -15,7 +15,8 @@ window.onload = function () {
 
       data.forEach((article) => {
         let articleCard = document.createElement("div");
-        articleCard.className = "service";
+        articleCard.className = "article-card";
+
         articleCard.dataset.id = article.id; // Ajoutez l'ID de l'article comme attribut de données
 
         // Utilisez les littéraux de gabarit pour créer le HTML de la carte d'article
