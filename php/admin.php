cf8707d9f1ef5680c052c2914c3fe8676208622e
McSon2 <contact@techassistance.fr>
2023-06-24 02:05:25 +0200

    modified:   php/admin.php

--- php/admin.php
+++ php/admin.php
@@ -24,3 +24,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header('Location: admin.php');
     exit;
 }
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Administration du blog</title>
+</head>
+<body>
+    <h1>Ajouter un nouvel article</h1>
+    <form action="admin.php" method="post">
+        <label for="titre">Titre:</label><br>
+        <input type="text" id="titre" name="titre"><br>
+        <label for="tags">Tags (séparés par des virgules):</label><br>
+        <input type="text" id="tags" name="tags"><br>
+        <label for="contenu">Contenu:</label><br>
+        <textarea id="contenu" name="contenu"></textarea><br>
+        <input type="submit" value="Ajouter l'article">
+    </form>
+</body>
+</html>
