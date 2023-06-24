45ff790dc6331fd4b002dd4c00ddd714728c2835
McSon2 <contact@techassistance.fr>
2023-06-24 02:44:25 +0200

    modified:   css/admin.css 	modified:   php/admin.php

--- php/admin.php
+++ php/admin.php
@@ -4,11 +4,20 @@ require_once 'db_config.php';
 // Connexion à la base de données
 $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
 
+global $password_article;
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Récupération des données du formulaire
     $titre = $_POST['titre'];
     $tags = $_POST['tags'];
     $contenu = $_POST['contenu'];
+    $password = $_POST['password'];
+
+    // Vérifiez le mot de passe
+    if ($password !== $password_article) {
+    echo "Mot de passe incorrect.";
+    exit;
+    }
 
     // Préparation de la requête SQL
     $query = $db->prepare("INSERT INTO articles (titre, date_publication, tags, contenu) VALUES (:titre, NOW(), :tags, :contenu)");
