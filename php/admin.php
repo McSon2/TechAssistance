a84c9c14f988a56401e4714ca106d55f1204049b
McSon2 <contact@techassistance.fr>
2023-06-24 02:03:48 +0200

    modified:   php/admin.php

--- php/admin.php
+++ php/admin.php
@@ -1,22 +1,26 @@
 <?php
 require_once 'db_config.php';
+
 // Connexion à la base de données
 $db = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
 
-// Récupération des données du formulaire
-$titre = $_POST['titre'];
-$tags = $_POST['tags'];
-$contenu = $_POST['contenu'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupération des données du formulaire
+    $titre = $_POST['titre'];
+    $tags = $_POST['tags'];
+    $contenu = $_POST['contenu'];
 
-// Préparation de la requête SQL
-$query = $db->prepare("INSERT INTO articles (titre, date_publication, tags, contenu) VALUES (:titre, NOW(), :tags, :contenu)");
+    // Préparation de la requête SQL
+    $query = $db->prepare("INSERT INTO articles (titre, date_publication, tags, contenu) VALUES (:titre, NOW(), :tags, :contenu)");
 
-// Exécution de la requête SQL
-$query->execute([
-    'titre' => $titre,
-    'tags' => $tags,
-    'contenu' => $contenu
-]);
+    // Exécution de la requête SQL
+    $query->execute([
+        'titre' => $titre,
+        'tags' => $tags,
+        'contenu' => $contenu
+    ]);
 
-echo "L'article a été ajouté avec succès.";
-?>
+    // Redirection vers la même page pour éviter le renvoi des données POST lors d'un rafraîchissement de la page
+    header('Location: admin.php');
+    exit;
+}
